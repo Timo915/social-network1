0f51d8f0e827d98392ce1dfaccd5622b1d8fdb8a
Timo915 <kraken907@mail.ru>
2024-11-08 23:26:47 +0300

    и

--- models/video.js
+++ models/video.js
@@ -1,11 +1,10 @@
 const mongoose = require('mongoose');
 
 const videoSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, required: true },
-    title: { type: String, required: true },
-    url: { type: String, required: true },
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    path: { type: String, required: true }, // Путь к видеофайлу
     createdAt: { type: Date, default: Date.now }
-    // другие поля...
 });
 
-module.exports = mongoose.model('Video', videoSchema);
+const Video = mongoose.model('Video', videoSchema);
+module.exports = Video;
